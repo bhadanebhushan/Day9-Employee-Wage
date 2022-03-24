4b68727c8d4d51961d6fd2dfe1a91147c36351c0
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-03-24 15:42:05 +0530

    Welcome

---
+++ src/com/employee/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.employee;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage computation program");
+
+	}
+
+}
